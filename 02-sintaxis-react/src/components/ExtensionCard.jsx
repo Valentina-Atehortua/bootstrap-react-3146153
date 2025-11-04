601f534248513af7dc5e7827272634fd9225fcb5
Valentina-Atehortua <vatehortua228@gmail.com>
2025-11-04 13:23:07 -0500

    cambios

--- 02-sintaxis-react/src/components/ExtensionCard.jsx
+++ 02-sintaxis-react/src/components/ExtensionCard.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import "../stylesheets/ExtensionCard.css"
 
 export const ExtensionCard = () => {
   return (
-    <div>ExtensionCard</div>
+    <>
+    <div className="general">
+      <div className="informacion">
+        <div className="icono">
+          <div className="icon"></div>
+          </div>
+          <div className="texto">
+            <h1 className="titulo">DevLens</h1>
+            <p className="desc">Quickly inspect page layouts and visualize element boundaries.</p>
+            </div>
+            </div>
+            <div className="remove">
+              <div className="remove1">Remove</div>
+              <div className="boton">💿</div>
+                </div>
+                </div>
+    </>
   )
 }

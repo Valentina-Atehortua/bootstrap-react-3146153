8fca1ed9e11d5ef215f182ecd2b3ec98fa794827
Valentina-Atehortua <vatehortua228@gmail.com>
2025-11-05 13:15:11 -0500

    Aplicación tarjetas finalizada

--- 02-sintaxis-react/src/components/ExtensionCard.jsx
+++ 02-sintaxis-react/src/components/ExtensionCard.jsx
@@ -1,23 +1,23 @@
 import "../stylesheets/ExtensionCard.css"
 
-export const ExtensionCard = () => {
+export const ExtensionCard = ({cardTitle, cardImage, cardDescription}) => {
   return (
     <>
-    <div className="general">
-      <div className="informacion">
-        <div className="icono">
-          <div className="icon"></div>
+      <div className="general">
+        <div className="informacion">
+          <div className="icono">
+            <div className="icon">{cardImage}</div>
           </div>
           <div className="texto">
-            <h1 className="titulo">DevLens</h1>
-            <p className="desc">Quickly inspect page layouts and visualize element boundaries.</p>
-            </div>
-            </div>
-            <div className="remove">
-              <div className="remove1">Remove</div>
-              <div className="boton">💿</div>
-                </div>
-                </div>
+            <h1 className="titulo">{cardTitle}</h1>
+            <p className="desc">{cardDescription}</p>
+          </div>
+        </div>
+        <div className="remove">
+          <div className="remove1">Remove</div>
+          <div className="boton">💿</div>
+        </div>
+      </div>
     </>
   )
 }

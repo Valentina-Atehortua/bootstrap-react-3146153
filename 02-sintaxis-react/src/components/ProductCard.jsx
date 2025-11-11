56fadf537acf4c81095289479ce24d68a8aec06a
Valentina-Atehortua <vatehortua228@gmail.com>
2025-11-11 13:17:20 -0500

    secciòn card terminado

--- 02-sintaxis-react/src/components/ProductCard.jsx
+++ 02-sintaxis-react/src/components/ProductCard.jsx
@@ -1,7 +1,7 @@
 export const ProductCard = () => {
     return (
-        <div className="card" style={{width: "18rem"}}>
-            <img src="..." className="card-img-top" alt="..." />
+        <div className="card" >
+            <div className="bg-secondary rounded-circle m-auto mt-3" style={{width: "80px", height: "80px"}}></div>
             <div className="card-body text-center">
                 <h5 className="card-title">Card title</h5>
                 <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>

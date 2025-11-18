a147bcb34112ca39d17d070b9a0e8a3418df5b57
Valentina-Atehortua <vatehortua228@gmail.com>
2025-11-18 13:25:10 -0500

    avances

--- 03-metrika/src/components/CuadroCard.jsx
+++ 03-metrika/src/components/CuadroCard.jsx
@@ -1,25 +1,33 @@
 export const CuadroCard = () => {
     return (
         <>
-            <div className="container text-center bg-danger bg-gradient">
-                <div className="row row-cols-2 row-cols-lg-5 g-2 g-lg-3">
-                    
+            <div className="container text-center">
+                <div className="row">
+
                     <div className="col">
-                        <div className="p-3">Row column</div>
+                        <div className="row">
+                            <div className="col-6 p-3 text-white bg-opacity-75">
+                                <div className="bg-success h-100 py-4">Row column</div>
+                            </div>
+                            <div className="col-6 p-3 bg-success text-white">
+                                 <div className="bg-success h-100 py-4">Row column</div>
+                            </div>
+                            <div className="col-12 p-3 mt-2 bg-danger bg-gradient text-white">Row column</div>
+                        </div>
                     </div>
                     <div className="col">
-                        <div className="p-3">Row column</div>
+                        <div className="p-3 bg-info  text-white">Row column</div>
                     </div>
                     <div className="col">
-                        <div className="p-3">Row column</div>
-                    </div>
-                    <div className="col">
-                        <div className="p-3">Row column</div>
-                    </div>
-                    <div className="col">
-                        <div className="p-3">Row column</div>
+                        <div className="row">
+                            <div className="col-12 p-3 bg-danger bg-opacity-75 text-white">Row column</div>
+                            <div className="col-6 p-3 mt-2 bg-warning text-white">Row column</div>
+                            <div className="col-6 p-3 mt-2 bg-warning bg-gradient bg-opacity-75 text-white">Row column</div>
+                        </div>
                     </div>
                 </div>
-            </div></>
+            </div>
+
+        </>
     )
 }

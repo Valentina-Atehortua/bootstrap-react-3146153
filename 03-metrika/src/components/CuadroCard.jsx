e2f04d9636ddd8d6c3923621db1312f0469f468c
Valentina-Atehortua <vatehortua228@gmail.com>
2025-11-19 13:24:59 -0500

    avance

--- 03-metrika/src/components/CuadroCard.jsx
+++ 03-metrika/src/components/CuadroCard.jsx
@@ -6,23 +6,45 @@ export const CuadroCard = () => {
 
                     <div className="col">
                         <div className="row">
-                            <div className="col-6 p-3 text-white bg-opacity-75">
-                                <div className="bg-success h-100 py-4">Row column</div>
+                            <div className="col-6 p-3 text-white">
+                                <div className="bg-success bg-opacity-75  h-100 py-1">
+                                    <img className="h-80" width="98" src="./libro.png" alt="" />
+                                    <p>About Use</p>
+                                </div>
                             </div>
-                            <div className="col-6 p-3 bg-success text-white">
-                                 <div className="bg-success h-100 py-4">Row column</div>
+                            <div className="col-6 p-3 text-white">
+                                 <div className="bg-success d-flex flex-column align-items-center justify-content-center h-100 py-1">
+                                    <img className="h-99" width="100" src="./mesa.png" alt="" />
+                                    <p>Our Team</p>
+                                 </div>
                             </div>
-                            <div className="col-12 p-3 mt-2 bg-danger bg-gradient text-white">Row column</div>
+                            <div className="col-12 p-3  text-white">
+                                <div className="bg-danger bg-gradient h-100 py-1">
+                                     <img className="h-60" width="80" src="./flecha.webp" alt="" />
+                                    <p>Resume</p>
+                                </div>
+                            </div>
                         </div>
                     </div>
                     <div className="col">
-                        <div className="p-3 bg-info  text-white">Row column</div>
+                        <div className="p-3 h-100 text-white">
+                            <div className="bg-info  h-100 py-5">Row column</div>
+                        </div>
                     </div>
                     <div className="col">
                         <div className="row">
-                            <div className="col-12 p-3 bg-danger bg-opacity-75 text-white">Row column</div>
-                            <div className="col-6 p-3 mt-2 bg-warning text-white">Row column</div>
-                            <div className="col-6 p-3 mt-2 bg-warning bg-gradient bg-opacity-75 text-white">Row column</div>
+                            <div className="col-12 p-3">
+                                <div className="bg-danger bg-opacity-75 text-white h-100 py-1">
+                                     <img className="h-99" width="90" src="./contacto.png" alt="" />
+                                    <p>Contacts</p>
+                                </div>
+                            </div>
+                            <div className="col-6 p-3 text-white">
+                                <div className="bg-warning h-100 py-5">Row column</div>
+                            </div>
+                            <div className="col-6 p-3 text-white">
+                                 <div className="bg-warning bg-gradient bg-opacity-75 h-100 py-5">Row column</div>
+                            </div>
                         </div>
                     </div>
                 </div>

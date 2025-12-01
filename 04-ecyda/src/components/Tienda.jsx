7791b527ca1b27911b0bf2c6620cee07554cb284
Valentina-Atehortua <vatehortua228@gmail.com>
2025-12-01 13:22:55 -0500

    actualización

--- 04-ecyda/src/components/Tienda.jsx
+++ 04-ecyda/src/components/Tienda.jsx
@@ -1,3 +1,4 @@
+import "../tienda.css"
 import { Header } from "./Header"
 
 export const Tienda = () => {
@@ -9,24 +10,19 @@ export const Tienda = () => {
       />
 
       <div className="banner position-relative">
-        <div className="circulo position-absolut"> <img src="./circulo.gif" alt="" /></div>
-        
-        <div>
-
+        <img className="img1 position-absolute p-2" width="190" src="./circulo.gif" alt="" />
+        <img className="img2 position-absolute p-2" width="190" src="./circulo.gif" alt="" />
+        <div className="ban position-relative p-4 h-100 d-flex justify-content-center">
+          <img className="ban h-100" src="./banner.gif" alt="" />
         </div>
-        <div className="circulo"></div>
       </div>
 
-      <section>
-        <h1>
-          <p></p>
-        </h1>
-      </section>
+      <div className="cuadro p-5 d-flex justify-content-center h-100" width="100">
+        <img className="img3 position-absolute" src="./cuadros.gif" alt="" />
+        <p className="tex position-relative">Bienvenido a la tienda de ECYDA, tu espacio digital para descubrir productos unicos, seleccionados con oasion y pensados para ti. Aqui, 
+        la calidad se encuentra con la comodidad de comprar desde donde estes. Explora, elige y deja que lo mejor llegue a tu puerta. </p>
+      </div>
 
-      <section class="alg-prod">
-        ProductCard
-      
-      </section>
 
     </>
   )
